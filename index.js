189f89979ee122dee1268bb36f4856eaf3a6ea86
Veronica Giraldo <veronica9893@gmail.com>
2018-01-16 01:52:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value })
 }
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
- const newObj = Object.assign({}, object)
+ 
 
   delete newObj[key]
 
